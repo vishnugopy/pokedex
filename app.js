cbfcfa4bd080e4e86f4f7c46189030280fc3bcbe
vishnugopy <vishnugoby0010@gmail.com>
2021-02-16 12:11:48 +0100

    jesuispro

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ let pokeBackImage = document.querySelector(".poke-back-image");
 let pokeWeight = document.querySelector(".poke-weight");
 let pokeHeight = document.querySelector(".poke-height");
 let listItem = document.querySelectorAll(".list-item")
+let typeOne = document.querySelector(".poke-type-one")
+let typeTwo = document.querySelector(".poke-type-two")
 //buttons
 let leftButton = document.querySelector(".left-button");
 let rightButton = document.querySelector(".right-button");
 let next ;
 let previous ;
 
-mainScreen.classList.remove("hide");
+
 
 let fetch_config = {
     method: "GET",
@@ -47,7 +49,8 @@ fetch( apilink , fetch_config)
                
                     let str = "";
                     for (let i = 0; i < arr.length; i++) {  
-                        str = i+1 + "." + arr[i]["name"];
+
+                        str = arr[i]["name"];
 
                        listItem[i].innerHTML = str ;
                     }
@@ -85,4 +88,67 @@ leftButton.addEventListener("click" , ()=>{
 window.addEventListener("load" , () =>{
     getpokies();
 })
+
+
+listItem.forEach(element => {
+    element.addEventListener("click",()=>{
+        console.log(element.textContent);
+
+// Ici on configure notre requete (method, en-tetes, corps)
+
+
+// Initialise la requete http avec l'url de la ressource et les configurations définis ci-dessus
+fetch("https://pokeapi.co/api/v2/pokemon/"+element.textContent)
+.then(function (response) {
+
+    // On appelle la method json a partir de l'objet response pour parser les données renvoyer par l'API
+    response.json()
+        .then(function (data) {
+            if (response.status == 400) {
+                console.log(data);
+                // gestion erreur données envoyer a la requette
+            }
+            else if (response.status == 403) {
+                console.log(data);
+                // gestion erreur authentification
+            }
+            else {
+                console.log(data);
+                // ici on peut exploiter nos donnée
+               mainScreen.classList.remove("hide");
+               pokeName.innerHTML = data["name"];     
+               pokeId.innerHTML = data["id"];       
+               pokeFrontImage.src = data["sprites"]["front_default"];
+               pokeBackImage.src = data["sprites"]["back_default"];
+               pokeWeight.innerHTML = data["weight"];            
+               pokeHeight.innerHTML = data["height"];
+               
+               if (data["types"].length == 2) {
+                typeOne.innerHTML = data["types"][0]["type"]["name"]
+                typeTwo.innerHTML = data["types"][1]["type"]["name"]
+                mainScreen.className = '';
+                mainScreen.classList.add("main-screen" , typeTwo.innerHTML);
+               } else {
+                typeOne.innerHTML = data["types"][0]["type"]["name"]
+                typeTwo.style.display = "none";
+                mainScreen.className = '';
+                mainScreen.classList.add("main-screen" , typeOne.innerHTML);
+               
+               }
+
+            }
+        })
+        .catch(function (data_parsing_error) {
+            console.log(data_parsing_error);
+        })
+})
+.catch(function(server_errors) {
+    // Cas erreur server (API)
+    console.log(server_errors);
+})
+
+
+
+    })
+});
 
